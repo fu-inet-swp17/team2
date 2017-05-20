5a986060c06220732c02d5b8a4dce9af054b3a93
cone <cone@zedat.fu-berlin.de>
2017-05-20 17:45:41 +0200

    Erster Versuch eine Client-Server kommunikation aufzubauen

---
+++ server-emulator/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "net/sock/udp.h"
+#include "shell.h"
+
+uint8_t buf[128];
+
+int main(void) {
+	// printf("This is the server-emulator on %s.\n", RIOT_BOARD);
+	
+	// TODO herausfinden was genau 'shell_run' tut
+	// char line_buf[SHELL_DEFAULT_BUFSIZE];
+	// shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+	// TODO warum funktioniert SOCK_IPV6_EP_ANY nicht?
+	sock_udp_ep_t local = SOCK_IPV6_EP_ANY;
+	
+	/*{
+		.family = AF_INET6,
+		.netif = SOCK_ADDR_ANY_NETIF
+	};*/
+	
+	sock_udp_t sock;
+	local.port = 2017;
+	if(sock_udp_create(&sock, &local, NULL, 0) < 0) {
+		puts("Error creating UDP sock");
+		return EXIT_FAILURE;
+	}
+	
+	while(true) {
+		ssize_t res;
+		if((res = sock_udp_recv(&sock, buf, sizeof(buf), SOCK_NO_TIMEOUT, NULL)) >= 0) {
+			printf("%.128s\n", buf);
+		}
+	}
+
+    return EXIT_SUCCESS;
+}
