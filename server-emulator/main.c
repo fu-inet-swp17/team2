523b919aefcc07e4bd9ccaedfc27f00b3324dbe8
cone <cone@zedat.fu-berlin.de>
2017-05-23 17:53:52 +0200

    Button handler registriert

--- server-emulator/main.c
+++ server-emulator/main.c
@@ -3,20 +3,32 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include "periph/gpio.h"
+#include "net/ipv6/addr.h"
 #include "net/sock/udp.h"
+#include "net/gnrc/ipv6/netif.h"
 #include "shell.h"
+#include "board.h"
 
-uint8_t buf[128];
+typedef struct SensorBoard {
+	ipv6_addr_t addr;
+} SensorBoard;
+
+void button_handler(void* args) {
+	printf("hipp hipp hurra\n");
+}
 
 int main(void) {
 	printf("This is the server-emulator on %s.\n", RIOT_BOARD);
-
-	sock_udp_ep_t local = SOCK_IPV6_EP_ANY;
 	
-	/*{
-		.family = AF_INET6,
-		.netif = SOCK_ADDR_ANY_NETIF
-	};*/
+	// funktioniert nur mit GPIO_IN_PU
+	// mit GPIO_RISING wird der Handler 2 mal bei Boardstart aufgerufen,
+	// Grund unbekannt 
+	gpio_init_int(BUTTON_GPIO, GPIO_IN_PU, GPIO_FALLING, button_handler, NULL);
+	printf("Button activated\n");
+	
+	/*
+	sock_udp_ep_t local = SOCK_IPV6_EP_ANY;
 	
 	sock_udp_t sock;
 	local.port = 2017;
@@ -33,10 +45,11 @@ int main(void) {
 			printf("%.128s\n", buf);
 		}
 	}
+	*/
 	
-	// TODO herausfinden was genau 'shell_run' tut
+	// TODO shell-commands bauen?
 	char line_buf[SHELL_DEFAULT_BUFSIZE];
 	shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
-	
+    
     return EXIT_SUCCESS;
 }
