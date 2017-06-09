313ade6ba06164511e5482b6d981caa6d6c0c6b9
Adrian GHC <adrian.ghc@outlook.com>
2017-06-09 18:42:08 +0200

    Change indentation style from tabs to four spaces in order to respect RIOT coding style conventions

--- board-app/main.c
+++ board-app/main.c
@@ -21,8 +21,8 @@
  * #return[bool]: true if prefix is a prefix of str, false else
  */
 static inline bool startsWith(const char* str, const char* prefix) {
-	char* substr = strstr(str, prefix);
-	return substr == str;
+    char* substr = strstr(str, prefix);
+    return substr == str;
 }
 
 
@@ -40,67 +40,67 @@ sock_udp_ep_t server_ep;
 // Array mit den CoAP Ressourcen
 // Die Ressourcen muessen in alphabetischer Reihnfolge eingefuegt werden!!!
 static const coap_resource_t se_resources[] = {
-	// ressource-path, ressource-type, response-handler
-	{"/se-app/sensors", COAP_GET, sensors_handler},
-	// {"/se-app/humid", COAP_GET, humid_handler},
-	// {"/se-app/temp", COAP_GET, temp_handler},
+    // ressource-path, ressource-type, response-handler
+    {"/se-app/sensors", COAP_GET, sensors_handler},
+    // {"/se-app/humid", COAP_GET, humid_handler},
+    // {"/se-app/temp", COAP_GET, temp_handler},
 };
 
 // TODO sind die listener so korrekt initialisiert?
 // listener für gcoap
 static gcoap_listener_t sensors_listener = {
-	(coap_resource_t*)&se_resources[0],
-	sizeof(se_resources) / sizeof(se_resources[0]),
-	NULL
+    (coap_resource_t*)&se_resources[0],
+    sizeof(se_resources) / sizeof(se_resources[0]),
+    NULL
 };
 
 /*static gcoap_listener_t humid_listener = {
-	(coap_resource_t*)&se_resources[1],
-	sizeof(se_resources) / sizeof(se_resources[1]),
-	NULL
+    (coap_resource_t*)&se_resources[1],
+    sizeof(se_resources) / sizeof(se_resources[1]),
+    NULL
 };
 
 static gcoap_listener_t temp_listener = {
-	(coap_resource_t*)&se_resources[2],
-	sizeof(se_resources) / sizeof(se_resources[2]),
-	NULL
+    (coap_resource_t*)&se_resources[2],
+    sizeof(se_resources) / sizeof(se_resources[2]),
+    NULL
 };*/
 
 // handler der angesprochen werden sollte wenn, eine anfrage nach den Sensoren
 // kommmt
 static ssize_t sensors_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len) {
-	gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
-	size_t payload_size = (size_t)snprintf(
-		(char*)pdu->payload, 
-		len,
-		"%s",
-		"temp-humid"
-	);
-	puts("responed");
-	return gcoap_finish(pdu, payload_size, COAP_FORMAT_TEXT);
+    gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+    size_t payload_size = (size_t)snprintf(
+        (char*)pdu->payload, 
+        len,
+        "%s",
+        "temp-humid"
+    );
+    puts("responed");
+    return gcoap_finish(pdu, payload_size, COAP_FORMAT_TEXT);
 }
 /*
 static ssize_t temp_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len) {
-	gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
-	// Temperatur-Sensorwert mit SAUL abfragen und in pdu->payload schreiben
-	// Anzahl der Bytes speichern und an gcoap_finish übergeben
-	size_t payload_size = 0;
-	return gcoap_finish(pdu, payload_size, COAP_FORMAT_NONE);
+    gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+    // Temperatur-Sensorwert mit SAUL abfragen und in pdu->payload schreiben
+    // Anzahl der Bytes speichern und an gcoap_finish übergeben
+    size_t payload_size = 0;
+    return gcoap_finish(pdu, payload_size, COAP_FORMAT_NONE);
 }
 
 static ssize_t humid_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len) {
-	gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
-	// Luftfeuchtigkeit-Sensorwert mit SAUL abfragen und in pdu->payload schreiben
-	// Anzahl der Bytes speichern und an gcoap_finish übergeben
-	size_t payload_size = 0;
-	return gcoap_finish(pdu, payload_size, COAP_FORMAT_NONE);
+    gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+    // Luftfeuchtigkeit-Sensorwert mit SAUL abfragen und in pdu->payload schreiben
+    // Anzahl der Bytes speichern und an gcoap_finish übergeben
+    size_t payload_size = 0;
+    return gcoap_finish(pdu, payload_size, COAP_FORMAT_NONE);
 }
 */
 /**********************************COAP STUFF**********************************/
 
 static inline void sensors_test(void) {
-	puts("\n##############################################");
-	puts("############## SAUL sensor test ##############\n");
+    puts("\n##############################################");
+    puts("############## SAUL sensor test ##############\n");
 
     saul_reg_t* dev = saul_reg;
 
@@ -108,100 +108,100 @@ static inline void sensors_test(void) {
         phydat_t res;
         int dim = saul_reg_read(dev, &res);
         printf("%s of type %s returns the following data: \n", dev->name, saul_class_to_str(dev->driver->type));
-		phydat_dump(&res, dim);
+        phydat_dump(&res, dim);
         if (dev->next == NULL) {
             break;
         }
         dev = dev->next;
     };
 
-	puts("##############################################\n");
+    puts("##############################################\n");
 }
 
 int main(void) {
     printf("Smart environment app on %s\n", RIOT_BOARD);
 
-	saul_reg_t* dev = saul_reg;
+    saul_reg_t* dev = saul_reg;
 
-	if (dev == NULL) {
-		puts("The SAUL registry does not contain any devices.");
-	} else {
-		printf("The SAUL registry contains the following devices: ");
-		while(1) {
-			printf("%s", dev->name);
-			if (dev->next == NULL) {
-				break;
-			}
-			printf(", ");
-			dev = dev->next;
-		};
-		printf("\n");
-	}
+    if (dev == NULL) {
+        puts("The SAUL registry does not contain any devices.");
+    } else {
+        printf("The SAUL registry contains the following devices: ");
+        while(1) {
+            printf("%s", dev->name);
+            if (dev->next == NULL) {
+                break;
+            }
+            printf(", ");
+            dev = dev->next;
+        };
+        printf("\n");
+    }
 
-	sensors_test();
+    sensors_test();
 
-	// ff02::1 -> addr für link-local broadcast
-	ipv6_addr_t addr;
-	// char prefix[IPV6_ADDR_MAX_STR_LEN];
-	// my_strncpy(prefix, , IPV6_ADDR_MAX_STR_LEN);
-	ipv6_addr_from_str(&addr, "fe80::");
+    // ff02::1 -> addr für link-local broadcast
+    ipv6_addr_t addr;
+    // char prefix[IPV6_ADDR_MAX_STR_LEN];
+    // my_strncpy(prefix, , IPV6_ADDR_MAX_STR_LEN);
+    ipv6_addr_from_str(&addr, "fe80::");
 
-	ipv6_addr_to_str(own_addr, &addr, IPV6_ADDR_MAX_STR_LEN);
+    ipv6_addr_to_str(own_addr, &addr, IPV6_ADDR_MAX_STR_LEN);
 
-	ipv6_addr_t* out = NULL;
-	gnrc_ipv6_netif_find_by_prefix(&out, &addr);
-	
-	ipv6_addr_to_str(own_addr, out, IPV6_ADDR_MAX_STR_LEN);
-	printf("own ipv6 addr: %s\n", own_addr);
+    ipv6_addr_t* out = NULL;
+    gnrc_ipv6_netif_find_by_prefix(&out, &addr);
+    
+    ipv6_addr_to_str(own_addr, out, IPV6_ADDR_MAX_STR_LEN);
+    printf("own ipv6 addr: %s\n", own_addr);
 
-	sock_udp_ep_t remote = SOCK_IPV6_EP_ANY;
-	remote.port = SERVER_CONN_PORT;
-	ipv6_addr_from_str((ipv6_addr_t *)&remote.addr.ipv6, "ff02::1");
-	
-	char intro_msg[CLIENT_INIT_MSG_LEN];
-	snprintf(intro_msg, CLIENT_INIT_MSG_LEN, "%s %s", client_id, own_addr);
-	
-	if(sock_udp_send(NULL, intro_msg, CLIENT_INIT_MSG_LEN, &remote) < 0) {
-		puts("Error sending intro message!");
-		return EXIT_FAILURE;
-	}
-	
-	uint8_t resp_buf[SERVER_RESP_MSG_LEN];
-	sock_udp_ep_t local = SOCK_IPV6_EP_ANY;
-	local.port = CLIENT_PORT; // empfangssocket also auf eigenen Port stellen
-	sock_udp_t sock;
-	sock_udp_create(&sock, &local, NULL, 0);
-	do {
-		ssize_t res = sock_udp_recv(
-			&sock,
-			resp_buf,
-			sizeof(resp_buf),
-			SOCK_NO_TIMEOUT,
-			NULL
-		);
-		
-		if(res < 0) {
-			puts("Error during receive!");
-			continue;
-		}
-		
-	} while(!startsWith((const char*)resp_buf, server_id));
-	
-	char* se_addr_str = strchr((const char*)resp_buf, ' ');
-	se_addr_str++; // addr_str zeigt sonst auf das Leerzeichen!
-	printf("server found on: %s\n", se_addr_str);
+    sock_udp_ep_t remote = SOCK_IPV6_EP_ANY;
+    remote.port = SERVER_CONN_PORT;
+    ipv6_addr_from_str((ipv6_addr_t *)&remote.addr.ipv6, "ff02::1");
+    
+    char intro_msg[CLIENT_INIT_MSG_LEN];
+    snprintf(intro_msg, CLIENT_INIT_MSG_LEN, "%s %s", client_id, own_addr);
+    
+    if(sock_udp_send(NULL, intro_msg, CLIENT_INIT_MSG_LEN, &remote) < 0) {
+        puts("Error sending intro message!");
+        return EXIT_FAILURE;
+    }
+    
+    uint8_t resp_buf[SERVER_RESP_MSG_LEN];
+    sock_udp_ep_t local = SOCK_IPV6_EP_ANY;
+    local.port = CLIENT_PORT; // empfangssocket also auf eigenen Port stellen
+    sock_udp_t sock;
+    sock_udp_create(&sock, &local, NULL, 0);
+    do {
+        ssize_t res = sock_udp_recv(
+            &sock,
+            resp_buf,
+            sizeof(resp_buf),
+            SOCK_NO_TIMEOUT,
+            NULL
+        );
+        
+        if(res < 0) {
+            puts("Error during receive!");
+            continue;
+        }
+        
+    } while(!startsWith((const char*)resp_buf, server_id));
+    
+    char* se_addr_str = strchr((const char*)resp_buf, ' ');
+    se_addr_str++; // addr_str zeigt sonst auf das Leerzeichen!
+    printf("server found on: %s\n", se_addr_str);
 
-	// TODO listener für alle Sensoren registrieren
-	
-	gcoap_init(); // kA ob die Reihenfolge stimmt
-	gcoap_register_listener(&sensors_listener); // test-listener
-	// gcoap_register_listener(&temp_listener);
-	// gcoap_register_listener(&humid_listener);
-	puts("Coap gedingst");
-	
-	// shell starten damit Application nicht terminiert
-	char line_buf[SHELL_DEFAULT_BUFSIZE];
-	shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
-		
+    // TODO listener für alle Sensoren registrieren
+    
+    gcoap_init(); // kA ob die Reihenfolge stimmt
+    gcoap_register_listener(&sensors_listener); // test-listener
+    // gcoap_register_listener(&temp_listener);
+    // gcoap_register_listener(&humid_listener);
+    puts("Coap gedingst");
+    
+    // shell starten damit Application nicht terminiert
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+        
     return EXIT_SUCCESS;
 }
--- server-emulator/main.c
+++ server-emulator/main.c
@@ -20,8 +20,8 @@
 #define TIMEOUT (uint32_t)2000000
 
 typedef struct SensorBoard {
-	bool init;
-	char addr[IPV6_ADDR_MAX_STR_LEN];
+    bool init;
+    char addr[IPV6_ADDR_MAX_STR_LEN];
 } SensorBoard;
 
 // gefährlich! sollte eigentlich gemutext werden
@@ -42,8 +42,8 @@ uint8_t req_buf[128];
  * #return[bool]: true wenn prefix is a prefix of str, false else
  */
 static inline bool startsWith(const char* str, const char* prefix) {
-	char* substr = strstr(str, prefix);
-	return substr == str;
+    char* substr = strstr(str, prefix);
+    return substr == str;
 }
 
 /*
@@ -54,13 +54,13 @@ static inline bool startsWith(const char* str, const char* prefix) {
  * #return[size_t]: Anzahl geschriebener Zeichen
  */
 static inline size_t my_strncpy(char* dst, const char* src, size_t num) {
-	size_t i;
-	for(i=0; src[i] && i<(num-1); i++) {
-		dst[i] = src[i];
-	}
-	
-	dst[i] = 0;
-	return i;
+    size_t i;
+    for(i=0; src[i] && i<(num-1); i++) {
+        dst[i] = src[i];
+    }
+    
+    dst[i] = 0;
+    return i;
 }
 
 
@@ -70,15 +70,15 @@ coap_pkt_t pdu;
 uint8_t coap_buff[GCOAP_PDU_BUF_SIZE];
 
 static void sensors_resp_handler(unsigned req_state, coap_pkt_t* pdu) {
-	if (req_state == GCOAP_MEMO_TIMEOUT) {
-		printf("timeout for msg ID %02u\n", coap_get_id(pdu));
-		return;
-	} else if (req_state == GCOAP_MEMO_ERR) {
-		printf("error in response\n");
-		return;
-	}
-	
-	printf("Request: %02u\nSensors: %s\n\n", coap_get_id(pdu), pdu->payload);
+    if (req_state == GCOAP_MEMO_TIMEOUT) {
+        printf("timeout for msg ID %02u\n", coap_get_id(pdu));
+        return;
+    } else if (req_state == GCOAP_MEMO_ERR) {
+        printf("error in response\n");
+        return;
+    }
+    
+    printf("Request: %02u\nSensors: %s\n\n", coap_get_id(pdu), pdu->payload);
 }
 
 /**********************************COAP STUFF**********************************/
@@ -87,41 +87,41 @@ static void sensors_resp_handler(unsigned req_state, coap_pkt_t* pdu) {
  * printf schwierig, sorgt für Stackoverflow
  */
 static void button_handler(void* args) {
-	puts("Requesting data from boards...");
-	
-	for(uint8_t i=0; i<MAX_BOARD_NUM; i++) {		
-		if(!boards[i].init) {
-			printf("Slot %u not used.\n", i);
-			continue;
-		}
-		
-		gcoap_request(
-			&pdu,
-			coap_buff,
-			GCOAP_PDU_BUF_SIZE,
-			COAP_GET,
-			"/se-app/sensors"
-		);
-		
-		sock_udp_ep_t board_ep;
-		board_ep.family = AF_INET6;
-		board_ep.netif = SOCK_ADDR_ANY_NETIF;
-		board_ep.port = GCOAP_PORT;
-		ipv6_addr_from_str((ipv6_addr_t *)&board_ep.addr.ipv6, boards[i].addr);
-		size_t bytes_sent = gcoap_req_send2(
-			coap_buff,
-			GCOAP_PDU_BUF_SIZE,
-			&board_ep,
-			sensors_resp_handler
-		);
-		// puts("Request send");
-		printf(
-			"Send request %u to board %u. Size: %u\n",
-			coap_get_id(&pdu),
-			i, 
-			bytes_sent
-		);
-	}
+    puts("Requesting data from boards...");
+    
+    for(uint8_t i=0; i<MAX_BOARD_NUM; i++) {        
+        if(!boards[i].init) {
+            printf("Slot %u not used.\n", i);
+            continue;
+        }
+        
+        gcoap_request(
+            &pdu,
+            coap_buff,
+            GCOAP_PDU_BUF_SIZE,
+            COAP_GET,
+            "/se-app/sensors"
+        );
+        
+        sock_udp_ep_t board_ep;
+        board_ep.family = AF_INET6;
+        board_ep.netif = SOCK_ADDR_ANY_NETIF;
+        board_ep.port = GCOAP_PORT;
+        ipv6_addr_from_str((ipv6_addr_t *)&board_ep.addr.ipv6, boards[i].addr);
+        size_t bytes_sent = gcoap_req_send2(
+            coap_buff,
+            GCOAP_PDU_BUF_SIZE,
+            &board_ep,
+            sensors_resp_handler
+        );
+        // puts("Request send");
+        printf(
+            "Send request %u to board %u. Size: %u\n",
+            coap_get_id(&pdu),
+            i, 
+            bytes_sent
+        );
+    }
 }
 
 
@@ -129,174 +129,174 @@ static void button_handler(void* args) {
  * Deamon der im Hintergrund läuft und neue Boards registriert
  */
 static void* connect_thread_handler(void* args) {
-	(void)args;
-	
-	sock_udp_ep_t local = SOCK_IPV6_EP_ANY;
-	
-	sock_udp_t sock;
-	local.port = SERVER_CONN_PORT;
-	bool sock_ready = false;
-	if(sock_udp_create(&sock, &local, NULL, 0) < 0) {
-		fputs("Error creating UDP sock!\n", stderr);
-		sock_ready = false;
-	} else {
-		puts("Successfull createt UDP socket.");
-		puts("Waiting for boards...");
-		sock_ready = true;
-	}
-	
-	while(sock_ready) {
-		ssize_t res = sock_udp_recv(
-			&sock,
-			conn_buf,
-			CLIENT_INIT_MSG_LEN,
-			SOCK_NO_TIMEOUT,
-			NULL
-		);
-		
-		if(res < 0) {
-			fprintf(
-				stderr,
-				"Error during \"sock_udp_recv\": %d!\nAboarting.",
-				res
-			);
-			sock_ready = false;
-		} else if((res) >= 0) {
-			if(!startsWith((const char*)conn_buf, client_id)) {
-				continue;
-			}
-		
-			char* addr_str = strchr((const char*)conn_buf, ' ');
-			addr_str++; // addr_str zeigt sonst auf das Leerzeichen!
-				
-			size_t board_index;
-			for(board_index=0;
-				board_index<MAX_BOARD_NUM && boards[board_index].init;
-				board_index++
-			);
-				
-			if(board_index >= MAX_BOARD_NUM) {
-				fputs("Max nr. of boards registered!\n", stderr);
-				break; // der Thread beendet sich!!!
-			}
-			boards[board_index].init = true;
-			my_strncpy(
-				boards[board_index].addr,
-				addr_str,
-				IPV6_ADDR_MAX_STR_LEN
-			);
-				
-			sock_udp_ep_t board_ep;
-			board_ep.family = AF_INET6;
-			board_ep.netif = SOCK_ADDR_ANY_NETIF;
-			board_ep.port = CLIENT_PORT;
-			ipv6_addr_from_str(
-				(ipv6_addr_t *)&board_ep.addr.ipv6,
-				boards[board_index].addr
-			);
-					
-			char resp_msg[SERVER_RESP_MSG_LEN];
-			snprintf(
-				resp_msg,
-				SERVER_RESP_MSG_LEN,
-				"%s %s",
-				server_id,
-				own_addr
-			);
-					
-			res = sock_udp_send(
-				NULL,
-				resp_msg,
-				SERVER_RESP_MSG_LEN,  
-				&board_ep
-			);
-				
-			if(res == -EAFNOSUPPORT) {
-				fputs("-EAFNOSUPPORT\n", stderr);
-			} else if(res == -EHOSTUNREACH) {
-				fputs("-EHOSTUNREACH\n", stderr);
-			} else if(res == -EINVAL) {
-				fputs("-EINVAL\n", stderr);
-			} else if(res == -ENOMEM) {
-				fputs("-ENOMEM\n", stderr);
-			} else if(res == -ENOTCONN) {
-				fputs("-ENOTCONN\n", stderr);
-			} else if(res < 0) {
-				fputs("error\n", stderr);
-			} else {
-				printf("Board found on: %s\n", addr_str);
-			}
-				
-			xtimer_sleep(10);
-			
-			gcoap_request(
-				&pdu,
-				coap_buff,
-				GCOAP_PDU_BUF_SIZE,
-				COAP_GET,
-				"/se-app/sensors"
-			);
+    (void)args;
+    
+    sock_udp_ep_t local = SOCK_IPV6_EP_ANY;
+    
+    sock_udp_t sock;
+    local.port = SERVER_CONN_PORT;
+    bool sock_ready = false;
+    if(sock_udp_create(&sock, &local, NULL, 0) < 0) {
+        fputs("Error creating UDP sock!\n", stderr);
+        sock_ready = false;
+    } else {
+        puts("Successfull createt UDP socket.");
+        puts("Waiting for boards...");
+        sock_ready = true;
+    }
+    
+    while(sock_ready) {
+        ssize_t res = sock_udp_recv(
+            &sock,
+            conn_buf,
+            CLIENT_INIT_MSG_LEN,
+            SOCK_NO_TIMEOUT,
+            NULL
+        );
+        
+        if(res < 0) {
+            fprintf(
+                stderr,
+                "Error during \"sock_udp_recv\": %d!\nAboarting.",
+                res
+            );
+            sock_ready = false;
+        } else if((res) >= 0) {
+            if(!startsWith((const char*)conn_buf, client_id)) {
+                continue;
+            }
+        
+            char* addr_str = strchr((const char*)conn_buf, ' ');
+            addr_str++; // addr_str zeigt sonst auf das Leerzeichen!
+                
+            size_t board_index;
+            for(board_index=0;
+                board_index<MAX_BOARD_NUM && boards[board_index].init;
+                board_index++
+            );
+                
+            if(board_index >= MAX_BOARD_NUM) {
+                fputs("Max nr. of boards registered!\n", stderr);
+                break; // der Thread beendet sich!!!
+            }
+            boards[board_index].init = true;
+            my_strncpy(
+                boards[board_index].addr,
+                addr_str,
+                IPV6_ADDR_MAX_STR_LEN
+            );
+                
+            sock_udp_ep_t board_ep;
+            board_ep.family = AF_INET6;
+            board_ep.netif = SOCK_ADDR_ANY_NETIF;
+            board_ep.port = CLIENT_PORT;
+            ipv6_addr_from_str(
+                (ipv6_addr_t *)&board_ep.addr.ipv6,
+                boards[board_index].addr
+            );
+                    
+            char resp_msg[SERVER_RESP_MSG_LEN];
+            snprintf(
+                resp_msg,
+                SERVER_RESP_MSG_LEN,
+                "%s %s",
+                server_id,
+                own_addr
+            );
+                    
+            res = sock_udp_send(
+                NULL,
+                resp_msg,
+                SERVER_RESP_MSG_LEN,  
+                &board_ep
+            );
+                
+            if(res == -EAFNOSUPPORT) {
+                fputs("-EAFNOSUPPORT\n", stderr);
+            } else if(res == -EHOSTUNREACH) {
+                fputs("-EHOSTUNREACH\n", stderr);
+            } else if(res == -EINVAL) {
+                fputs("-EINVAL\n", stderr);
+            } else if(res == -ENOMEM) {
+                fputs("-ENOMEM\n", stderr);
+            } else if(res == -ENOTCONN) {
+                fputs("-ENOTCONN\n", stderr);
+            } else if(res < 0) {
+                fputs("error\n", stderr);
+            } else {
+                printf("Board found on: %s\n", addr_str);
+            }
+                
+            xtimer_sleep(10);
+            
+            gcoap_request(
+                &pdu,
+                coap_buff,
+                GCOAP_PDU_BUF_SIZE,
+                COAP_GET,
+                "/se-app/sensors"
+            );
 
-			board_ep.port = GCOAP_PORT;
-				
-			size_t bytes_sent = gcoap_req_send2(
-				coap_buff,
-				GCOAP_PDU_BUF_SIZE,
-				&board_ep,
-				sensors_resp_handler
-			);
-			// puts("Request send");
-			printf(
-				"Send request %u to board %u. Size: %u\n",
-				coap_get_id(&pdu),
-				board_index, 
-				bytes_sent
-			);
-				
-		}
-	}
+            board_ep.port = GCOAP_PORT;
+                
+            size_t bytes_sent = gcoap_req_send2(
+                coap_buff,
+                GCOAP_PDU_BUF_SIZE,
+                &board_ep,
+                sensors_resp_handler
+            );
+            // puts("Request send");
+            printf(
+                "Send request %u to board %u. Size: %u\n",
+                coap_get_id(&pdu),
+                board_index, 
+                bytes_sent
+            );
+                
+        }
+    }
 
-	return NULL;
+    return NULL;
 }
 
 int main(void) {
-	printf("This is the server-emulator on \"%s\".\n", RIOT_BOARD);
-	
-	for(size_t i=0; i<MAX_BOARD_NUM; i++) {
-		boards[i].init = false;
-	}
-	
-	// zu suchendes Prefix in ipv6 umwandeln
-	ipv6_addr_t addr;
-	ipv6_addr_from_str(&addr, "fe80::");
+    printf("This is the server-emulator on \"%s\".\n", RIOT_BOARD);
+    
+    for(size_t i=0; i<MAX_BOARD_NUM; i++) {
+        boards[i].init = false;
+    }
+    
+    // zu suchendes Prefix in ipv6 umwandeln
+    ipv6_addr_t addr;
+    ipv6_addr_from_str(&addr, "fe80::");
 
-	// eigene link-local adresse anhand prefix suchen 
-	ipv6_addr_t* ll_addr = NULL;
-	gnrc_ipv6_netif_find_by_prefix(&ll_addr, &addr);
-	
-	ipv6_addr_to_str(own_addr, ll_addr, IPV6_ADDR_MAX_STR_LEN);
-	printf("Link-local address: \"%s\".\n", own_addr);
-	
-	// funktioniert nur mit GPIO_IN_PU
-	// mit GPIO_RISING wird der Handler 2 mal bei Boardstart aufgerufen,
-	// Grund unbekannt
-	gpio_init_int(BUTTON_GPIO, GPIO_IN_PU, GPIO_FALLING, button_handler, NULL);
-	puts("Button activated.");
-	
-	puts("Coap gedingst.");
-	thread_create(
-		connect_stack,
-		sizeof(connect_stack),
-		THREAD_PRIORITY_MAIN-1,
-		THREAD_CREATE_STACKTEST,
-		connect_thread_handler,
-		NULL,
-		"connect thread"
-	);
-		
-	// TODO shell-commands bauen?
-	char line_buf[SHELL_DEFAULT_BUFSIZE];
-	shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+    // eigene link-local adresse anhand prefix suchen 
+    ipv6_addr_t* ll_addr = NULL;
+    gnrc_ipv6_netif_find_by_prefix(&ll_addr, &addr);
+    
+    ipv6_addr_to_str(own_addr, ll_addr, IPV6_ADDR_MAX_STR_LEN);
+    printf("Link-local address: \"%s\".\n", own_addr);
+    
+    // funktioniert nur mit GPIO_IN_PU
+    // mit GPIO_RISING wird der Handler 2 mal bei Boardstart aufgerufen,
+    // Grund unbekannt
+    gpio_init_int(BUTTON_GPIO, GPIO_IN_PU, GPIO_FALLING, button_handler, NULL);
+    puts("Button activated.");
+    
+    puts("Coap gedingst.");
+    thread_create(
+        connect_stack,
+        sizeof(connect_stack),
+        THREAD_PRIORITY_MAIN-1,
+        THREAD_CREATE_STACKTEST,
+        connect_thread_handler,
+        NULL,
+        "connect thread"
+    );
+        
+    // TODO shell-commands bauen?
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
     
     return EXIT_SUCCESS;
 }
