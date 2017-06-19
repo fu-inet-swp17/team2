0b7d235c3023b1febb43bfc2123d664e5d613f9a
Niclas Kristek <nkristek@users.noreply.github.com>
2017-06-19 16:17:47 +0200

    Transfer SenML implementation into its own repository
    You can visit the new repository here: https://github.com/nkristek/go-senml
    By outsourcing that library, it should be easier to use SenML parsing in a separate Golang project

--- RiotPi/src/riot/riot_client.go
+++ RiotPi/src/riot/riot_client.go
@@ -7,7 +7,7 @@ package riot
 import (
 	"../config"                 // config for Configuration struct
 	"../data"                   // data for Device struct
-	"../senml"                  // SenML parsing
+	"github.com/nkristek/go-senml"                  // SenML parsing
 	"errors"                    // create errors
 	"fmt"                       // sprintf
 	"github.com/dustin/go-coap" // coap
@@ -60,15 +60,81 @@ func GetSensorData(device data.Device, deviceResources []data.DeviceResource, co
 
 		log.Debugf("response payload: " + string(resp.Payload[:]))
 
-		parsedResponse, err := senml.ParseMessage(resp.Payload)
+		message, err := senml.ParseBytes(resp.Payload, senml.JSON)
 		if err != nil {
 			log.Error("parsing SenML: ", err)
 			return nil, err
 		}
 
+		resolvedMessage, err := senml.Resolve(message)
+		if err != nil {
+			log.Error("resolving SenML: ", err)
+			return nil, err
+		}
+
 		log.Debugf("successfully parsed SenML")
-		senml.LogMessage(*parsedResponse)
+		logSenMLMessage(resolvedMessage)
 	}
 
 	return nil, nil
+}
+
+func logSenMLMessage(message senml.SenMLMessage) {
+  if len(message.Records) == 0 {
+    log.Debugf("no records")
+    return
+  }
+
+  for _, record := range message.Records {
+    log.Debugf("-----")
+    if record.BaseName != nil {
+      log.Debugf("BaseName: %s", *record.BaseName)
+    }
+    if record.BaseTime != nil {
+      log.Debugf("BaseTime: %f", *record.BaseTime)
+    }
+    if record.BaseUnit != nil {
+      log.Debugf("BaseUnit: %s", *record.BaseUnit)
+    }
+    if record.BaseValue != nil {
+      log.Debugf("BaseValue: %f", *record.BaseValue)
+    }
+    if record.BaseSum != nil {
+      log.Debugf("BaseSum: %f", *record.BaseSum)
+    }
+    if record.Version != nil {
+      log.Debugf("Version: %d", *record.Version)
+    }
+    if record.Name != nil {
+      log.Debugf("Name: %s", *record.Name)
+    }
+    if record.Unit != nil {
+      log.Debugf("Unit: %s", *record.Unit)
+    }
+    if record.Value != nil {
+      log.Debugf("Value: %f", *record.Value)
+    }
+    if record.StringValue != nil {
+      log.Debugf("StringValue: %s", *record.StringValue)
+    }
+    if record.BoolValue != nil {
+      log.Debugf("BoolValue: %t", *record.BoolValue)
+    }
+    if record.DataValue != nil {
+      log.Debugf("DataValue: %s", *record.DataValue)
+    }
+    if record.Sum != nil {
+      log.Debugf("Sum: %f", *record.Sum)
+    }
+    if record.Time != nil {
+      log.Debugf("Time: %f", *record.Time)
+    }
+    if record.UpdateTime != nil {
+      log.Debugf("UpdateTime: %f", *record.UpdateTime)
+    }
+    if record.Link != nil {
+      log.Debugf("Link: %s", *record.Link)
+    }
+    log.Debugf("-----")
+  }
 }
