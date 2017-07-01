032beead45c6932bb89270fd5167474b68ecd05f
Adrian GHC <adrian.ghc@outlook.com>
2017-07-01 18:30:28 +0200

    board-app: Add function for future use that calculates floor or ceiling of float value without explicitly using the float value but only the int value and the scale

--- board-app/floatint.c
+++ board-app/floatint.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 
 void int_to_floatstring(char* buf, int16_t val, int8_t scale) {
@@ -21,4 +22,19 @@ void int_to_floatstring(char* buf, int16_t val, int8_t scale) {
         }
     }
     
+}
+
+int32_t int_from_float(int16_t val, int8_t scale) {
+    if (scale < 0) {
+        int16_t val2 = val % (int16_t) pow(10, -scale);
+        int16_t val1 = (val - val2) / (int16_t) pow(10, -scale);
+        if (val2 >= (int16_t) pow(10, -scale) / 2) {
+            val1 += 1;
+        }
+        return val1;
+    } else if (scale == 0) {
+        return val;
+    } else {
+        return val * (int16_t) pow(10, scale);
+    }
 }
--- board-app/include/floatint.h
+++ board-app/include/floatint.h
@@ -1,2 +1,4 @@
 
 void int_to_floatstring(char* buf, int16_t val, int8_t scale);
+
+int16_t int_from_float(int16_t val, int8_t scale);
