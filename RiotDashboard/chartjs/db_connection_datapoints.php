f44cfe3c92b6966139e00b9e6ddf9582973db805
Adrian GHC <adrian.ghc@outlook.com>
2017-07-15 00:43:21 +0200

    RiotDashboard: Finalize chart generation + plus other changes
    - Charts are now generated dynamically with Google Charts for each sensor/unit
    - Add CSV parsing for StringValue
    - Remove sequence diagram and protocol sections of website as up-to-date documentation can be found on the GitHub page
    - Additional minor code refactoring and cleanup

--- RiotDashboard/chartjs/db_connection_datapoints.php
+++ RiotDashboard/chartjs/db_connection_datapoints.php
@@ -7,33 +7,47 @@ include '/config.php';
 
 //get connection
 $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+mysqli_set_charset($mysqli, "utf8");
 
-if(!$mysqli){
+if(!$mysqli) {
 	die("Connection failed: " . $mysqli->error);
 }
 
-$nameQuery = sprintf("SELECT DISTINCT Name FROM DataTable");
+$nameQuery = "SELECT DISTINCT Name FROM ".DB_TABLE;
 $nameResult = $mysqli->query($nameQuery);
 $resultArray = array();
-if (is_array($nameResult) || is_object($nameResult))
-{
+if (is_array($nameResult) || is_object($nameResult)) {
 	// iterate over all different sensor names
 	foreach ($nameResult as $name) {
-		// get datapoints from that sepcific sensor
-		$query = "SELECT * FROM DataTable WHERE Name = '".$name["Name"]."' ORDER BY Time";
-		$result = $mysqli->query($query);
-		
-		// go over results from that specific datapoint
-		$dataArray = array();
-		if (is_array($result) || is_object($result)) {
-			while($row = $result->fetch_assoc()) {
-				array_push($dataArray, $row);
+		// get all units for that sensor
+		$unitQuery = "SELECT DISTINCT Unit FROM ".DB_TABLE." WHERE Name = '".$name["Name"]."' ORDER BY Unit ASC";
+		$unitResult = $mysqli->query($unitQuery);
+		$innerResultArray = array();
+		if (is_array($unitResult) || is_object($unitResult)) {
+			// iterate over all different units for that sensor
+			foreach ($unitResult as $unit) {
+				// get datapoints from that sepcific sensor
+				$query = "SELECT * FROM ".DB_TABLE." WHERE Name = '".$name["Name"]."' AND Unit = '".$unit["Unit"]."' ORDER BY Time";
+				$result = $mysqli->query($query);
+				
+				// go over results from that specific datapoint
+				$dataArray = array();
+				if (is_array($result) || is_object($result)) {
+					while($row = $result->fetch_assoc()) {
+						array_push($dataArray, $row);
+					}
+					$result->close();
+				}
+				
+				// add datapoints of the current unit to the result array
+				array_push($innerResultArray, array($unit["Unit"] => $dataArray));
 			}
-			$result->close();
+			
+			$unitResult->close();
 		}
 		
 		// add datapoints of the current sensor to the result array
-		array_push($resultArray, array($name["Name"] => $dataArray));
+		array_push($resultArray, array($name["Name"] => $innerResultArray));
 	}
 	
 	$nameResult->close();
--- RiotDashboard/chartjs/js/chart.js
+++ RiotDashboard/chartjs/js/chart.js
@@ -1,33 +1,47 @@
 $(document).ready(function(){
 	$.ajax({
-		url: "http://localhost/scrollspy/chartjs/db_connection_datapoints.php",
+		url: "http://localhost/RiotDashboard/chartjs/db_connection_datapoints.php",
 		method: "GET",
 		success: function(data) {
 			for (var sensorTuple in data) {
-				for(var sensorName in data[sensorTuple]) {
-					var ID = [];
-					var Unit = [];
-					var Value = [];
-					var StringValue = [];
-					var BoolValue = [];
-					var DataValue = [];
-					var Sum = [];
-					var Time = [];
-					var Link = [];
-					
-					for(var datapoint in data[sensorTuple][sensorName]) {
-						ID.push(data[sensorTuple][sensorName][datapoint].ID);
-						Unit.push(data[sensorTuple][sensorName][datapoint].Unit);
-						Value.push(data[sensorTuple][sensorName][datapoint].Value);
-						StringValue.push(data[sensorTuple][sensorName][datapoint].StringValue);
-						BoolValue.push(data[sensorTuple][sensorName][datapoint].BoolValue);
-						DataValue.push(data[sensorTuple][sensorName][datapoint].DataValue);
-						Sum.push(data[sensorTuple][sensorName][datapoint].Sum);
-						Time.push(data[sensorTuple][sensorName][datapoint].Time);
-						Link.push(data[sensorTuple][sensorName][datapoint].Link);
+				for (var sensorName in data[sensorTuple]) {
+					for (var unitTuple in data[sensorTuple][sensorName]) {
+						for (var unitName in data[sensorTuple][sensorName][unitTuple]) {
+							var ID = [];
+							var Value = [];
+							var StringValue = [];
+							var BoolValue = [];
+							var DataValue = [];
+							var Sum = [];
+							var Time = [];
+							var Link = [];
+							
+							for (var datapoint in data[sensorTuple][sensorName][unitTuple][unitName]) {
+								ID.push(parseInt(data[sensorTuple][sensorName][unitTuple][unitName][datapoint].ID));
+								var curValue;
+								if ((curValue = data[sensorTuple][sensorName][unitTuple][unitName][datapoint].Value) != null) {
+									Value.push(parseFloat(curValue));
+								}
+								var curStringValue;
+								if ((curStringValue = data[sensorTuple][sensorName][unitTuple][unitName][datapoint].StringValue) != null) {
+									StringValue.push(curStringValue.split(",").map(parseFloat));
+								}
+								var curBoolValue;
+								if ((curBoolValue = data[sensorTuple][sensorName][unitTuple][unitName][datapoint].BoolValue) != null) {
+									BoolValue.push(parseInt(curBoolValue));
+								}
+								var curDataValue;
+								if ((curDataValue = data[sensorTuple][sensorName][unitTuple][unitName][datapoint].DataValue) != null) {
+									DataValue.push(curDataValue);
+								}
+								Sum.push(data[sensorTuple][sensorName][unitTuple][unitName][datapoint].Sum);
+								Time.push(new Date(data[sensorTuple][sensorName][unitTuple][unitName][datapoint].Time));
+								Link.push(data[sensorTuple][sensorName][unitTuple][unitName][datapoint].Link);
+							}
+							
+							createChart(sensorName, unitName, Value, StringValue, BoolValue, DataValue, Time);
+						}
 					}
-					
-					createCanvas(sensorName, Value, StringValue, BoolValue, DataValue, Time);
 				}
 			}
 					
@@ -39,39 +53,39 @@ $(document).ready(function(){
 	});
 });
 
-function createCanvas(name, Value, StringValue, BoolValue, DataValue, Time) {
+function createChart(Name, Unit, Value, StringValue, BoolValue, DataValue, Time) {
 	var FinalData = [];
-	if (Value != []) {
-		FinalData = Value;
-	} else if (StringValue != []) {
-		FinalData = StringValue;
-	} else if (BoolValue != []) {
-		FinalData = BoolValue;
-	} else if (DataValue != []) {
+	if (Value.length != 0) {
+		FinalData = [Value];
+	} else if (StringValue.length != 0) {
+		FinalData = _.zip.apply(_, StringValue);
+	} else if (BoolValue.length != 0) {
+		FinalData = [BoolValue];
+	} else if (DataValue.length != 0) {
 		FinalData = DataValue;
 	}
 	
-	document.getElementById("sections").innerHTML += "<div id='section_" + name + "'><div id='chart-container_" + name + "'><canvas id='" + name + "'" + "></canvas></div></div>";
-	
-	//create chart
-	var chartdata = {
-		labels: Time,
-		datasets : [
-			{
-				label: name,
-				backgroundColor: "rgb(0,0,255)",
-				data: FinalData
-			}
-		]
-	};
+	document.getElementById("sections").innerHTML += "<div id='section_" + Name + "_" + Unit + "' style='height:500px; margin:100px 200px 100px 200px'></div>";
+	google.charts.load('current', {'packages':['corechart']});
+	google.charts.setOnLoadCallback(drawChart);
 
-	var ctx = document.getElementById(name).getContext('2d');
+	function drawChart() {
+		var data = new google.visualization.DataTable();
+		data.addColumn('datetime', 'Time');
+		var dataRow = [Time];
+		for (var i=0; i<FinalData.length; i++) {
+			data.addColumn('number', Unit);
+			dataRow.push(FinalData[i]);
+		}
+		data.addRows(_.zip.apply(_, dataRow));
 	
-	var lineGraph = new Chart(ctx, {
-		type: 'line',
-		data: chartdata
-	});
-	
-	//end of chart
-	
+		var options = {
+			title: Name,
+			legend: { position: 'bottom' }
+		};
+
+		var chart = new google.visualization.LineChart(document.getElementById("section_" + Name + "_" + Unit));
+		
+		chart.draw(data, options);
+	}
 }
