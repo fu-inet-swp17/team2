69f03d01cd0bc362ea9eac75f8ae73a48c845b15
Adrian GHC <adrian.ghc@outlook.com>
2017-07-15 13:44:07 +0200

    RiotDashboard: Add database table configuration to config.php

--- RiotDashboard/chartjs/config.php
+++ RiotDashboard/chartjs/config.php
@@ -1,10 +1,11 @@
 <?php
 
 //database
-define('DB_HOST', '');//database host 
-define('DB_USERNAME', '');//database username
-define('DB_PASSWORD', '');//database password
-define('DB_NAME', '');//database name
+define('DB_HOST', ''); //database host 
+define('DB_USERNAME', ''); //database username
+define('DB_PASSWORD', ''); //database password
+define('DB_NAME', ''); //database name
+define('DB_TABLE', ''); //database table name
 
 //get connection
 $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
